6e8eacd9ab3bcc64caca50d3eec72a1ac35d5b22
henteko <henteko07@gmail.com>
2011-10-29 01:01:31 +0900

    colors.xmlに、デザインの色を指定した

--- gen/com/teres/Liability2011/R.java
+++ gen/com/teres/Liability2011/R.java
@@ -10,26 +10,31 @@ package com.teres.Liability2011;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int actionbar_color=0x7f040000;
+        public static final int background_color=0x7f040001;
+        public static final int dashboard_text_color=0x7f040002;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int IndexButton=0x7f060003;
-        public static final int MapButton=0x7f060004;
-        public static final int TimeTableButton=0x7f060005;
-        public static final int Up_dataButton=0x7f060002;
-        public static final int actionbar_compat=0x7f060000;
-        public static final int imageView1=0x7f060001;
+        public static final int IndexButton=0x7f070003;
+        public static final int MapButton=0x7f070004;
+        public static final int TimeTableButton=0x7f070005;
+        public static final int Up_dataButton=0x7f070002;
+        public static final int actionbar_compat=0x7f070000;
+        public static final int imageView1=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int json=0x7f040002;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int json=0x7f050002;
     }
     public static final class style {
-        public static final int DashboardButton=0x7f050000;
+        public static final int DashboardButton=0x7f060000;
     }
 }
