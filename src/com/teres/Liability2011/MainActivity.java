c4b80c517d7da06a8ce278c1cf5f1dbad278d670
henteko <henteko07@gmail.com>
2011-11-08 01:59:47 +0900

    no change

--- src/com/teres/Liability2011/MainActivity.java
+++ src/com/teres/Liability2011/MainActivity.java
@@ -17,8 +17,10 @@ import com.teres.Liability2011.index.IndexActivity;
 import com.teres.Liability2011.timetable.TimeTableActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -27,6 +29,7 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
@@ -352,6 +355,21 @@ public class MainActivity extends Activity {
 			}
 
 		}).start();
+	}
+	
+	
+	
+	
+	// Backキー
+	public boolean onKeyDown(int keyCode, KeyEvent event,Bundle outState) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			super.onSaveInstanceState(outState);
+			outState.putString("hoge", "hoge");
+			super.onPause();
+			return false;
+		} else {
+			return super.onKeyDown(keyCode, event);
+		}
 	}
 
 }
